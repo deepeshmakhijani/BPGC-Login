f29d6a1cf2fdfda60e2d0648ff28c73df83ef479
Deepesh Makhijani <deepeshmak@gmail.com>
2016-10-28 21:56:40 +0530

    Initial Commit

---
+++ app/src/main/java/net/deepeshmakhijani/bpgclogin/MainActivity.java
@@ -0,0 +1,212 @@
+package net.deepeshmakhijani.bpgclogin;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String URL1 = "https://20.20.2.11/login.html";
+    private static final String URL2 = "https://10.1.0.10:8090/httpclient.html";
+    Button login_btn;
+    EditText user_data, pass_data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        HttpsTrustManager.allowAllSSL();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        login_btn = (Button) findViewById(R.id.login_btn);
+
+        user_data = (EditText) findViewById(R.id.user_data);
+        pass_data = (EditText) findViewById(R.id.pass_data);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login1();
+            }
+        });
+    }
+
+    private void login1() {
+        final String username = user_data.getText().toString().trim();
+        final String password = pass_data.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL1,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                params.put("buttonClicked", "4");
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void login2() {
+        final String username = user_data.getText().toString().trim();
+        final String password = pass_data.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL2,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                params.put("mode", "191");
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public static class HttpsTrustManager implements X509TrustManager {
+
+        private static final X509Certificate[] _AcceptedIssuers = new X509Certificate[]{};
+        private static TrustManager[] trustManagers;
+
+        public static void allowAllSSL() {
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+
+            });
+
+            SSLContext context = null;
+            if (trustManagers == null) {
+                trustManagers = new TrustManager[]{new HttpsTrustManager()};
+            }
+
+            try {
+                context = SSLContext.getInstance("TLS");
+                context.init(null, trustManagers, new SecureRandom());
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (KeyManagementException e) {
+                e.printStackTrace();
+            }
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(context
+                    .getSocketFactory());
+        }
+
+        @Override
+        public void checkClientTrusted(
+                java.security.cert.X509Certificate[] x509Certificates, String s)
+                throws java.security.cert.CertificateException {
+
+        }
+
+        @Override
+        public void checkServerTrusted(
+                java.security.cert.X509Certificate[] x509Certificates, String s)
+                throws java.security.cert.CertificateException {
+
+        }
+
+        public boolean isClientTrusted(X509Certificate[] chain) {
+            return true;
+        }
+
+        public boolean isServerTrusted(X509Certificate[] chain) {
+            return true;
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return _AcceptedIssuers;
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
